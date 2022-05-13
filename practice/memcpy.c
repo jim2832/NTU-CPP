d5990325f350a72fcac00b08dfb82a16b5596136
linebot0705 <jim880705@gmail.com>
2022-05-13 17:01:28 +0800

    update

---
+++ practice/memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    int a[5] = {1,5,7,2,3};
+    int b[5] = {0};
+
+    printf("Before memory copy:\n");
+    for(int i=0; i<sizeof(b)/sizeof(b[0]); i++){
+        printf("b[%d] = %d\n", i, b[i]);
+    }
+
+    printf("\n");
+    memcpy(b, a, sizeof(int)*5);
+
+    printf("Before memory copy:\n");
+    for(int i=0; i<sizeof(b)/sizeof(b[0]); i++){
+        printf("b[%d] = %d\n", i, b[i]);
+    }
+
+    return 0;
+}
