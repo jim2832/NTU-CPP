362adfaf0575dc1e860bd0eed37d7b18c9329032
linebot0705 <jim880705@gmail.com>
2022-05-15 17:52:07 +0800

    ok

--- practice/score/score.c
+++ practice/score/score.c
@@ -5,6 +5,8 @@ int main(void) {
     int sum = 0;
     int score[N];
     double avg;
+    int max;
+    int max_index;
 
     printf("Please enter your scores:");
     for(int i=0; i<N; i++){
@@ -22,12 +24,25 @@ int main(void) {
 
     printf("\n");
 
+    //the following is for printing the fail students
     printf("The following students are fail:\n");
     for(int i=0; i<N; i++){
         if(score[i] < 60){
-            printf("Student %d: %d\n",i ,score[i]);
+            printf("Student %d: %d\n",i+1 ,score[i]);
         }
     }
+    
+    printf("\n");
+
+    //the following is for finding the highest score
+    printf("The highest student and his/her score is:\n");
+    for(int i=0; i<N; i++){
+        if(max < score[i]){
+            max = score[i];
+            max_index = i;
+        }
+    }
+    printf("Student %d: %d\n", max_index+1, max);
 
     return 0;
 }
