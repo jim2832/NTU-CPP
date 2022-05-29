64f6ed63c3e693d43616ec33fa506d5030418c2c
linebot0705 <jim880705@gmail.com>
2022-05-29 22:37:15 +0800

    update

--- practice/char/char.c
+++ practice/char/char.c
@@ -2,10 +2,28 @@
 #include <stdlib.h>
 
 int main(void){
-    char ch;
+    char ch1;
+    char ch2;
 
-    ch = getchar();
-    putchar(ch);
+    /*
+    ch1 = getchar();
+    ch2 = getchar();
+
+    scanf(" %c", &ch1);
+    scanf(" %c", &ch2);
+    */
+
+    /*
+    putchar(ch1);
+    putchar(ch2);
+    putchar(65); //print "A"
+    putchar(165 -100); //print "A"
+    putchar(65+32); //print "a"
+    */
+
+    for(int i=0; i<128; i++){
+        printf("%3d: %c\n", i, i);
+    }
 
     return 0;
 }
